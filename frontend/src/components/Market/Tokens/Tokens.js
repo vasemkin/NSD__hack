f2d26f2a9b473b91bacdb6b944fe1f9d612504a8
vasemkin <vasemkin@ya.ru>
2021-05-16 06:08:49 +0300

    n.g.r.o.k

--- frontend/src/components/Market/Tokens/Tokens.js
+++ frontend/src/components/Market/Tokens/Tokens.js
@@ -5,47 +5,120 @@ import {
     Tr,
     Th,
     Td,
-    Button
+    Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    useToast,
+    Box,
+    FormControl,
+    FormLabel,
+    Input
  } from "@chakra-ui/react"
+
+import React, { useState } from "react"
 
 function Tokens(props) {
     const market = props.market
 
+    const toast = useToast()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    const [ currentToken, setCurrentToken ] = useState({
+        token : null
+    })
+
+    function triggerPurchase(token) {
+        onOpen()
+        setCurrentToken({
+            token : token
+        })
+    }
+
     return(
-        <Table variant="simple" size="md">
-            <Thead>
-                <Tr>
-                    <Th fontSize="14px" color="#000">Название</Th>
-                    <Th fontSize="14px" color="#000">Цена выплаты</Th>
-                    <Th fontSize="14px" color="#000">Дата погашения</Th>
-                    <Th fontSize="14px" color="#000">Конец аукциона</Th>
-                    <Th fontSize="14px" color="#000">Кол-во</Th>
-                    <Th fontSize="14px" color="#000">&nbsp;</Th>
-                </Tr>
-            </Thead>
-            <Tbody>
-                {market.marketTokens.length > 0
-                
-                ? market.marketTokens.map((token) => {
-                    return(
-                        <Tr key={token.name}>
-                            <Td>{token.name}</Td>
-                            <Td>{token.payoff}</Td>
-                            <Td>{token.expiryDate}</Td>
-                            <Td>{token.auctionEndDate}</Td>
-                            <Td>{token.totalCount}</Td>
-                            <Td>
-                                <Button variant="secondary" size="sm">Просмотр</Button>
-                            </Td>
-                        </Tr>
-                    )
-                })
-                :
-                null
-            }
-            </Tbody>
-        </Table>
-    )
+        <Box>
+            <Table variant="simple" size="md">
+                <Thead>
+                    <Tr>
+                        <Th fontSize="14px" color="#000">Название</Th>
+                        <Th fontSize="14px" color="#000">Цена выплаты</Th>
+                        <Th fontSize="14px" color="#000">Дата погашения</Th>
+                        <Th fontSize="14px" color="#000">Конец аукциона</Th>
+                        <Th fontSize="14px" color="#000">Кол-во</Th>
+                        <Th fontSize="14px" color="#000">&nbsp;</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {market.marketTokens.length > 0
+                    
+                    ? market.marketTokens.map((token) => {
+                        return(
+                            <Tr key={token.name}>
+                                <Td>{token.name}</Td>
+                                <Td>{token.payoff}</Td>
+                                <Td>{token.expiryDate}</Td>
+                                <Td>{token.auctionEndDate}</Td>
+                                <Td>{token.totalCount}</Td>
+                                <Td>
+                                    <Button onClick={() => {triggerPurchase(token)}} variant="secondary" size="sm">Купить</Button>
+                                </Td>
+                            </Tr>
+                        )
+                    })
+                    :
+                    null
+                }
+                </Tbody>
+            </Table>
+
+
+            <Modal size="sm" isOpen={isOpen} isCentered={true} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent borderRadius="30px" p="30px">
+                    <ModalHeader p="0" mb="2rem">Купить токен {currentToken.name}</ModalHeader>
+                    <ModalCloseButton mr="30px" mt="30px" maxWidth="none"/>
+                    <ModalBody p="0">
+
+                        <FormControl mb="1rem">
+                            <FormLabel mb="0">Количество</FormLabel>
+                            <Input 
+                                variant="flushed"
+                                // onChange={(e) => setRegisterAdmin({
+                                //     ...registerAdmin,
+                                //     uuid : e.target.value
+                                //     })
+                                // }
+                                type="text" />
+                        </FormControl> 
+
+                        <FormControl >
+                            <FormLabel mb="0">Ставка</FormLabel>
+                            <Input 
+                                variant="flushed"
+                                // onChange={(e) => setRegisterAdmin({
+                                //     ...registerAdmin,
+                                //     uuid : e.target.value
+                                //     })
+                                // }
+                                type="text" />
+                        </FormControl> 
+
+                    </ModalBody>
+
+                    <ModalFooter p="0" mt="2rem">
+                        <Button>Купить</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+    
+
+        </Box>
+        )
 }
 
 export default Tokens
